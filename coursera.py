786eefe14cb2217c2d6627f35ba5a33350b849f6
arseni-baranov <arseni-baranov@yandex.ru>
2017-01-06 05:26:48 +0500

    Перепишет проект с нуля

--- coursera.py
+++ coursera.py
@@ -1,119 +1,114 @@
-from requests import get
-from xml.dom import minidom
-from random import shuffle
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+import requests
+import json
+import random
 from bs4 import BeautifulSoup
-from dateutil.parser import parse
-
+from lxml import etree
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Font
 
-from re import findall
+
+XLS_HEADERFILL = PatternFill(start_color='E1E4D2', end_color='E1E4D2', fill_type='solid')
+XLS_TITLEFONT = Font(size=11, bold=True, color='000000')
 
 
-class Constants:
-    xml_feed = "https://www.coursera.org/sitemap~www~courses.xml"
-    courses_urls = []
-    key_names = ['Name', 'Language', 'Rating', 'Starts on', 'Duration (in weeks)']
-    class_names = ['.title', '.language-info', '.ratings-text']
-    all_courses = []
-    xls_headerfill = PatternFill(start_color='E1E4D2', end_color='E1E4D2', fill_type='solid')
-    xls_titlefont = Font(size=11, bold=True, color='000000')
+def get_random_courses(amount=20):
+    base_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    data = requests.get(base_url)
+    root = etree.fromstring(data.content)
+    name_spaces = {'urls': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+    urls_obj_list = root.xpath("//urls:loc", namespaces=name_spaces)
+    all_urls_list = [url.text for url in urls_obj_list]
+    random_20_urls = random.sample(all_urls_list, amount)
+    return random_20_urls
 
 
-def get_courses_list(amount=20, url_tag='loc'):
-
-    try:
-        xml_doc = minidom.parseString(get(Constants.xml_feed).content)
-        location_list = xml_doc.getElementsByTagName(url_tag)
-        shuffle(location_list)
-
-        Constants.courses_urls.extend([tag.firstChild.data
-                                       for index, tag in enumerate(location_list) if index < amount])
-
-    except Exception as error:
-        print('An error occurred while getting courses xml:', error)
-
-    return Constants.courses_urls
+def get_page(url):
+    response = requests.get(url)
+    page = response.text
+    soup = BeautifulSoup(page, 'lxml')
+    return soup
 
 
-def get_course_info(course_slug):
-
-    sleep_for = 30
-
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(1124, 850)
-    driver.get(course_slug)
-
-    just_wait = lambda: WebDriverWait(driver, sleep_for).\
-        until(EC.visibility_of_element_located((By.CLASS_NAME, "comfy")))
-
-    print("fetching course: {}, [{}/{}]".format
-         (course_slug, Constants.courses_urls.index(course_slug)+1, len(Constants.courses_urls)))
-
-    while 1:
+def get_course_language(soup):
+    class_table = "basic-info-table bt3-table bt3-table-striped bt3-table-bordered bt3-table-responsive"
+    table = soup.find('table', class_=class_table)
+    if table:
+        row_info = []
+        for row in table.find_all("tr")[:3]:
+            row_info.extend([cell.get_text(strip=True)
+                             for cell in row.find_all("td")])
         try:
-            just_wait()
-        except BaseException:
-            sleep_for += 10
-            just_wait()
-        else:
-            html = driver.execute_script("return document.getElementById('rendered-content').innerHTML")
-            driver.quit()
-            return html
+            language_index = row_info.index('Language') + 1
+            language = row_info[language_index]
+        except ValueError:
+            language = 'unknown'
+        return language
 
 
-def parse_course(html):
-
-    course = []
-    soup = BeautifulSoup(html, "html.parser")
-
-    get_element = lambda element: soup.select_one(element).get_text()
-
+def get_start_date(soup):
     try:
-        for class_ in Constants.class_names:
-            course.append(get_element(class_))
-    except:
-        course.append('Not rated yet')
-
-    try:
-        js_chunk = soup.find('script', type="application/ld+json").contents[0].strip()
-        dates = findall("\d{4}-\d{2}-\d{2}", js_chunk)
-    except:
-        course.extend(['Date unknown', 'Duration unknown'])
-    else:
-        course.extend([dates[0], int((parse(dates[1])-parse(dates[0])).days/7-1)])
-
-    Constants.all_courses.append(course)
+        data_from_script = soup.select(
+            'script[type="application/ld+json"]')[0].text
+        data_json = json.loads(data_from_script)
+        start_date = data_json['hasCourseInstance'][0]['startDate']
+        return start_date
+    except IndexError:
+        return None
 
 
-def output_courses_info_to_xlsx(filepath):
+def get_course_rating(soup):
+    rating = soup.find('div', class_='ratings-text bt3-visible-xs')
+    return None if rating is None else rating.text
+
+
+def get_course_title(soup):
+    title = soup.find('div', class_='title display-3-text')
+    return None if title is None else title.text
+
+
+def get_course_duration(soup):
+    duration = soup.findAll('div', class_='week-heading body-2-text')
+    return len(duration)
+
+
+def get_course_data(course_slug):
+    soup = get_page(course_slug)
+    return {'title': get_course_title(soup),
+            'startdate': get_start_date(soup),
+            'rating': get_course_rating(soup),
+            'duration': get_course_duration(soup),
+            'language': get_course_language(soup),
+            'url': course_slug}
+
+
+def output_courses_xlsx(all_courses_info, xls_output):
     wb = Workbook()
-    ws = wb.active
-    ws.append(Constants.key_names)
+    sheet = wb.active
+    sheet.append(['Title', 'Start date', 'Rating',
+                  'Duration in weeks', 'Language', 'Url'])
 
-    for course in Constants.all_courses:
-        ws.append(course)
-
-    for row in ws["A1:E1"]:
+    for row in sheet["A1:F1"]:
         for cell in row:
-            cell.fill = Constants.xls_headerfill
-            cell.font = Constants.xls_titlefont
+            cell.fill = XLS_HEADERFILL
+            cell.font = XLS_TITLEFONT
 
-    wb.save(filepath)
+    for course in all_courses_info:
+        course_info = [course['title'], course['startdate'], course['rating'],
+                       course['duration'], course['language'], course['url']]
+        sheet.append(course_info)
+    wb.save(xls_output)
 
 
 def main():
+    xls_output = input('Enter filename to save the xls file: ')
+    all_courses_info = []
+    urls_list = get_random_courses()
+    for url in urls_list:
+        course_info = get_course_data(url)
+        all_courses_info.append(course_info)
+    output_courses_xlsx(all_courses_info, xls_output)
+    print('the file has been successfully saved')
 
-    for course in get_courses_list():
-        parse_course(get_course_info(course))
-
-    output_courses_info_to_xlsx("courses.xlsx")
 
 if __name__ == '__main__':
     main()
